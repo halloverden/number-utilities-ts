9fa6b6839381daa261123bd29322aea18007a6cc
Erich Json Fosse <erich@halloverden.no>
2023-08-04 16:10:44 +0200

    ADD: Date utilities for nin

---
+++ src/countries/norway/utilities/nin-date-utilities.ts
@@ -0,0 +1,95 @@
+import { NinEntity } from '../types';
+import { checkDate } from '../../../international/utilities/date-utilities';
+
+/**
+ * Parses the birthdate from the given nin entity
+ *
+ * @param ninEntity
+ */
+export function getBirthDateFromNin(ninEntity: NinEntity): Date {
+  const day = getDayFromNin(ninEntity);
+  const month = getMonthFromNin(ninEntity);
+  const year = getYearFromNin(ninEntity.nin);
+
+  if (!checkDate(year, month, day)) {
+    throw new Error('Invalid date');
+  }
+  const d = new Date(year, month - 1, day);
+  d.setHours(0);
+  d.setMinutes(0);
+  d.setSeconds(0);
+  d.setMilliseconds(0);
+
+  return d;
+}
+
+/**
+ * Parses the day of the birthdate from the given nin entity
+ *
+ * @param ninEntity
+ */
+export function getDayFromNin(ninEntity: NinEntity): number {
+  const day = parseInt(ninEntity.nin.substring(0, 2), 10);
+
+  if (ninEntity.isDNumber()) {
+    return day - 40;
+  }
+
+  return day;
+}
+
+/**
+ * Parses the month of the birthdate from the given nin entity
+ *
+ * @param ninEntity
+ */
+export function getMonthFromNin(ninEntity: NinEntity): number {
+  const month = parseInt(ninEntity.nin.substring(2, 4), 10);
+
+  if (ninEntity.isHNumber()) {
+    return month - 40;
+  }
+
+  if (ninEntity.isNprSyntheticNin()) {
+    return month - 80;
+  }
+
+  return month;
+}
+
+/**
+ * Parses the year of the birthdate from the given nin entity
+ *
+ * @param ninEntity
+ */
+export function getYearFromNinEntity(ninEntity: NinEntity): number {
+  return getYearFromNin(ninEntity.nin);
+}
+
+/**
+ * Parses the year of the birthdate from the given nin
+ *
+ * @param nin
+ */
+export function getYearFromNin(nin: string): number {
+  const year = parseInt(nin.substring(4, 6), 10);
+  const ind = parseInt(nin.substring(6, 9), 10);
+
+  if (ind >= 0 && ind <= 499) {
+    return 1900 + year;
+  }
+
+  if (ind >= 500 && ind <= 749 && year >= 54 && year <= 99) {
+    return 1800 + year;
+  }
+
+  if (ind >= 500 && ind <= 999 && year >= 0 && year <= 39) {
+    return 2000 + year;
+  }
+
+  if (ind >= 900 && ind <= 999 && year >= 40 && year <= 99) {
+    return 1900 + year;
+  }
+
+  throw new Error('Invalid nin');
+}
---
+++ tests/countries/norway/utilities/nin-date-utilities.spec.ts
@@ -0,0 +1,84 @@
+import { getBirthDateFromNin } from '@norway/utilities';
+import { NinEntity } from '@norway/types';
+
+describe('nin-date-utilities.getBirthDateFromNin()', () => {
+  it('should return the correct birth date for 01026849833', () => {
+    expect.hasAssertions();
+    const d1 = getBirthDateFromNin(new NinEntity('01026849833'));
+    const cd1 = new Date(1968, 1, 1, 0, 0, 0, 0);
+    expect(d1).toBeTruthy();
+    expect(d1.toDateString()).toStrictEqual(cd1.toDateString());
+  });
+
+  it('should return the correct birth date for 01025454908', () => {
+    expect.hasAssertions();
+    const d2 = getBirthDateFromNin(new NinEntity('01025454908'));
+    const cd2 = new Date(1854, 1, 1, 0, 0, 0, 0);
+    expect(d2).toBeTruthy();
+    expect(d2.toDateString()).toStrictEqual(cd2.toDateString());
+  });
+
+  it('should return the correct birth date for 01021048557', () => {
+    expect.hasAssertions();
+    const d3 = getBirthDateFromNin(new NinEntity('01021048557'));
+    const cd3 = new Date(1910, 1, 1, 0, 0, 0, 0);
+    expect(d3).toBeTruthy();
+    expect(d3.toDateString()).toStrictEqual(cd3.toDateString());
+  });
+
+  it('should return the correct birth date for 24040499268', () => {
+    expect.hasAssertions();
+    const d4 = getBirthDateFromNin(new NinEntity('24040499268'));
+    const cd4 = new Date(2004, 3, 24, 0, 0, 0, 0);
+    expect(d4).toBeTruthy();
+    expect(d4.toDateString()).toStrictEqual(cd4.toDateString());
+  });
+
+  it('should return the correct birth date for 01038054070', () => {
+    expect.hasAssertions();
+    const d5 = getBirthDateFromNin(new NinEntity('01038054070'));
+    const cd5 = new Date(1880, 2, 1, 0, 0, 0, 0);
+    expect(d5).toBeTruthy();
+    expect(d5.toDateString()).toStrictEqual(cd5.toDateString());
+  });
+
+  it('should return the correct birth date for 01032078806', () => {
+    expect.hasAssertions();
+    const d6 = getBirthDateFromNin(new NinEntity('01032078806'));
+    const cd6 = new Date(2020, 2, 1, 0, 0, 0, 0);
+    expect(d6).toBeTruthy();
+    expect(d6.toDateString()).toStrictEqual(cd6.toDateString());
+  });
+
+  it('should return the correct birth date for 07031953000', () => {
+    expect.hasAssertions();
+    const d7 = getBirthDateFromNin(new NinEntity('07031953000'));
+    const cd7 = new Date(2019, 2, 7, 0, 0, 0, 0);
+    expect(d7).toBeTruthy();
+    expect(d7.toDateString()).toStrictEqual(cd7.toDateString());
+  });
+
+  it('should return the correct birth date for 41038058167', () => {
+    expect.hasAssertions();
+    const d8 = getBirthDateFromNin(new NinEntity('41038058167'));
+    const cd8 = new Date(1880, 2, 1, 0, 0, 0, 0);
+    expect(d8).toBeTruthy();
+    expect(d8.toDateString()).toStrictEqual(cd8.toDateString());
+  });
+
+  it('should return the correct birth date for 64040472086', () => {
+    expect.hasAssertions();
+    const d9 = getBirthDateFromNin(new NinEntity('64040472086'));
+    const cd9 = new Date(2004, 3, 24, 0, 0, 0, 0);
+    expect(d9).toBeTruthy();
+    expect(d9.toDateString()).toStrictEqual(cd9.toDateString());
+  });
+
+  it('should return the correct birth date for 47031999088', () => {
+    expect.hasAssertions();
+    const d10 = getBirthDateFromNin(new NinEntity('47031999088'));
+    const cd10 = new Date(2019, 2, 7, 0, 0, 0, 0);
+    expect(d10).toBeTruthy();
+    expect(d10.toDateString()).toStrictEqual(cd10.toDateString());
+  });
+});
