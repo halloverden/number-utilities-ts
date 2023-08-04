249b31fd911ce808abfe5e9c920dccc551bcbb20
Erich Json Fosse <erich@halloverden.no>
2023-08-04 16:03:00 +0200

    EDIT: Updates to conform with getMod11ControlDigit

--- src/countries/norway/validators/organization-number-validator.ts
+++ src/countries/norway/validators/organization-number-validator.ts
@@ -1,13 +1,20 @@
-import { getMod11ControlDigit } from '@international/utilities';
+import { getDefaultMod11WeightsForLength, getMod11ControlDigit } from '../../../international/utilities';
 
-export function validateOrganizationNumber(orgNumber: string) {
+export const ORG_NUMBER_WEIGHTS = getDefaultMod11WeightsForLength(8);
+
+/**
+ * Validates the given organization number
+ *
+ * @param orgNumber
+ */
+export function validateOrganizationNumber(orgNumber: string): boolean {
   orgNumber = orgNumber.replace(new RegExp(/ /g), '');
   if (!orgNumber || orgNumber.length !== 9) {
     return false;
   }
 
   return (
-    parseInt(orgNumber.charAt(orgNumber.length - 1), 10) ===
-    getMod11ControlDigit(orgNumber.substr(0, orgNumber.length - 1))
+    orgNumber.charAt(orgNumber.length - 1) ===
+    getMod11ControlDigit(orgNumber.substring(0, orgNumber.length - 1), ORG_NUMBER_WEIGHTS)
   );
 }
