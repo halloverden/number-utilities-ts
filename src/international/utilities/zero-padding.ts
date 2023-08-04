98db635a7e054a7925c159e3d241b6228d6fa352
Erich Json Fosse <erich@halloverden.no>
2023-08-04 15:48:22 +0200

    FIX: Returning unmodified value if it's longer than or equal to the specified length

--- src/international/utilities/zero-padding.ts
+++ src/international/utilities/zero-padding.ts
@@ -1,12 +1,18 @@
 /**
  * Returns a zero padded string with the length specified
+ * If length of incoming value is longer than or equal to the specified length,
+ * the value is returned unmodified
  *
  * @param value
  * @param length
  *
  * @return string
  */
-export function zeroPadding(value: string, length: number = 2): string {
+export function zeroPadding(value: string, length = 2): string {
+  if (value.length >= length) {
+    return value;
+  }
+
   while (value.length < length) {
     value = '0' + value;
   }
